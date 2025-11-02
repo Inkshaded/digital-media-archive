19321a968208b497df0273955e0a9a259f1ee2f1
Inkshaded <griffingreimes@gmail.com>
2025-11-02 14:41:21 -0800

    Added upload file button

--- ui/ui_start.py
+++ ui/ui_start.py
@@ -1,5 +1,11 @@
 import tkinter as tk
+from tkinter import filedialog
 
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                            title = "Select a File",
+                                            filetypes = (("Text files", "*.txt*"), 
+                                                        ("All files", "*.*")))
 
 class ArchiveUI:
     def __init__(self, root):
@@ -8,4 +14,6 @@ class ArchiveUI:
         self.root.geometry("900x700")
 
         # ~~~ (In Window) Title ~~~
-        tk.Label(root, text="Digital Media Archiver", font=("Arial", 18, "bold")).pack(pady=10)
+        tk.Label(root, text="Digital Media Archiving System", font=("Arial", 18, "bold")).pack(pady=10)
+
+        tk.Button(root, text="Upload Files", command=browseFiles).pack()
